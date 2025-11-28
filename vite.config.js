74641ce8e570c62f29a269cecf149f77c2d62cc6
Nattapong.m <nattapong.m@TCNB68212.local>
2025-11-28 19:28:21 +0700

    Fix Vite base path for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
+// vite.config.js
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
-  base: '/th8-ai-site-v2/', // ← ใช้ชื่อ repo
   plugins: [vue()],
+  base: '/th8-ai-site-v2/',  // 👈 สำคัญมาก ต้องตรงกับชื่อ repo
 })

df0422379e573b423c4ec6e0addf9e9b86c044a0
Nattapong.m <nattapong.m@TCNB68212.local>
2025-11-28 19:18:21 +0700

    change index.js

--- src/router/index.js
+++ src/router/index.js
@@ -109,8 +109,10 @@ const routes = [
 ]
 
 const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHistory(import.meta.env.BASE_URL),
   routes,
 })
 
+
 export default router
+
